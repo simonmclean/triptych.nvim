9b098d6b4a73306eb644df2bbc15f29f33cd7a97
Simon McLean <simonmclean@192.168.1.107>
2023-12-01 19:02:35 +0000

    script to run all tests

---
+++ run-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+all_tests_pass=true
+
+for test_path in ./lua/spec/*
+do
+  nvim --headless -c "PlenaryBustedFile $test_path" | grep "Failed" | grep -v "0" > /dev/null
+  if [ $? -eq 0 ]
+  then
+    all_tests_pass=false
+    echo "Tests failed in $test_path"
+  fi
+done
+
+if [ "$all_tests_pass" = false ]
+then
+  exit 1
+else
+  exit 0
+fi
