abd3d14968bd9e22ab86bca8e38ae7f8cf166115
Simon McLean <simonmclean@192.168.1.107>
2023-12-05 19:08:53 +0000

    debug

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,11 @@
 
 all_tests_pass=true
 
+echo "JIt"
+nvim --headless -c "lua vim.print(jit)"
+echo "VERSION"
+nvim --headless -c "lua vim.print(_VERSION)"
+
 for test_path in ./lua/spec/*
 do
   nvim --headless -c "PlenaryBustedFile $test_path" | grep "Failed" | grep -v "0" > /dev/null
