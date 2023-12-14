e6268fba2212c3693660bb023700c8485af0f909
Simon McLean <simonmclean@192.168.1.107>
2023-12-14 19:12:23 +0000

    improve validate script

--- validate.sh
+++ validate.sh
@@ -2,6 +2,17 @@
 
 # Use this script to test changes locally
 
+echo "Running tests..."
 nvim --headless -c 'PlenaryBustedDirectory lua/spec/'
+test_exit_code=$?
+if [ $test_exit_code -ne 0 ]; then
+  echo "❌ 1 or more tests failed";
+else
+  echo "✅ Tests passed";
+fi
+
+echo "Checking formatting..."
 npx @johnnymorganz/stylua-bin --check .
+
+echo "Check diagnostics..."
 ~/.local/share/nvim/mason/bin/lua-language-server --check .
