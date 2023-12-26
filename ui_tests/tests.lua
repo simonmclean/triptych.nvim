cfba17791e8a67858671849c868366017a51525e
Simon McLean <simonmclean@simons-mbp-2.home>
2023-12-26 22:36:20 +0000

    try a different char

--- ui_tests/tests.lua
+++ ui_tests/tests.lua
@@ -19,7 +19,7 @@ describe('triptych', function()
         },
         file_icons = {
           directory_icon = '+',
-          fallback_file_icon = '-',
+          fallback_file_icon = '*',
         },
       },
     }
@@ -35,17 +35,17 @@ describe('triptych', function()
   it('opens up with the expected lines', function()
     assert.same({
       '+ level_2_dir_1/',
-      '- level_2_file_1.java',
-      '- level_2_file_2.sh',
-      '- level_2_file_3.php',
+      '* level_2_file_1.java',
+      '* level_2_file_2.sh',
+      '* level_2_file_3.php',
     }, tu.get_lines 'parent')
     assert.same({
       '+ level_3_dir_1/',
-      '- level_3_file_1.java',
+      '* level_3_file_1.java',
     }, tu.get_lines 'primary')
     assert.same({
       '+ level_4_dir_1/',
-      '- level_4_file_1.js',
+      '* level_4_file_1.js',
     }, tu.get_lines 'child')
   end)
 
@@ -61,26 +61,26 @@ describe('triptych', function()
     assert.same({
       '+ level_1_dir_1/',
       '+ level_1_dir_2/',
-      '- level_1_file_1.js',
-      '- level_1_file_2.ts',
-      '- level_1_file_3.lua',
+      '* level_1_file_1.js',
+      '* level_1_file_2.ts',
+      '* level_1_file_3.lua',
     }, tu.get_lines 'parent')
     assert.same({
       '+ level_2_dir_1/',
-      '- level_2_file_1.java',
-      '- level_2_file_2.sh',
-      '- level_2_file_3.php',
+      '* level_2_file_1.java',
+      '* level_2_file_2.sh',
+      '* level_2_file_3.php',
     }, tu.get_lines 'primary')
     assert.same({
       '+ level_3_dir_1/',
-      '- level_3_file_1.java',
+      '* level_3_file_1.java',
     }, tu.get_lines 'child')
   end)
 
   it('handles nav right to a dir', function()
     tu.user_input 'l'
     assert.same({
-      '- level_5_file_1.lua',
+      '* level_5_file_1.lua',
     }, tu.get_lines 'child')
   end)
 end)
