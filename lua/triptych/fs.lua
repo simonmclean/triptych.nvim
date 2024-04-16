57fa7dcae9ca0c6a495ffa26ded2ec4551c50830
Simon McLean <simonmclean@192.168.1.101>
2024-04-16 20:32:40 +0100

    remove unused method

--- lua/triptych/fs.lua
+++ lua/triptych/fs.lua
@@ -1,6 +1,5 @@
 local u = require 'triptych.utils'
 local plenary_filetype = require 'plenary.filetype'
-local plenary_scandir = require 'plenary.scandir'
 local plenary_path = require 'plenary.path'
 local plenary_async = require 'plenary.async'
 
@@ -92,75 +91,6 @@ function M.get_path_details(_path, show_hidden, callback)
   end
 
   callback(tree)
-end
-
----@param _path string
----@param show_hidden boolean
----@param callback fun(path_details: PathDetails): nil
-function M.get_path_details_old(_path, show_hidden, callback)
-  local vim = _G.triptych_mock_vim or vim
-  local path = vim.fs.normalize(_path)
-
-  local tree = {
-    path = path,
-    display_name = vim.fs.basename(path),
-    dirname = nil, -- i.e. parent dir
-    is_dir = vim.fn.isdirectory(path) == 1,
-    filetype = nil,
-    children = {},
-  }
-
-  plenary_scandir.scan_dir_async(path, {
-    depth = 1,
-    add_dirs = true,
-    respect_gitignore = not show_hidden,
-    hidden = show_hidden,
-    silent = true,
-    on_exit = function(children)
-      ---@type { path: string, is_dir: boolean }[]
-      local children_with_type = u.eval(function()
-        local result = {}
-        for _, child in ipairs(children) do
-          table.insert(result, { path = child, is_dir = vim.fn.isdirectory(child) == 1 })
-        end
-        return result
-      end)
-
-      if vim.g.triptych_config.options.dirs_first then
-        table.sort(children_with_type, function(a, b)
-          if a.is_dir and not b.is_dir then
-            return true
-          end
-          if not a.is_dir and b.is_dir then
-            return false
-          end
-          return a.path < b.path
-        end)
-      end
-
-      for index, child in ipairs(children_with_type) do
-        tree.children[index] = {
-          path = child.path,
-          display_name = u.cond(child.is_dir, {
-            when_true = vim.fs.basename(child.path) .. '/',
-            when_false = vim.fs.basename(child.path),
-          }),
-          filetype = u.cond(child.is_dir, {
-            when_true = nil,
-            when_false = function()
-              return M.get_filetype_from_path(child.path)
-            end,
-          }),
-          basename = vim.fs.basename(child.path),
-          dirname = vim.fs.dirname(child.path),
-          is_dir = child.is_dir,
-          children = {},
-        }
-      end
-
-      callback(tree)
-    end,
-  })
 end
 
 return M
