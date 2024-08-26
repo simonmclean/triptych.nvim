a1bc81afa136d9a0a3c103fe8de8be963f4c1ed9
Simon McLean <simonmclean@192.168.1.116>
2024-08-26 23:05:28 +0100

    public events

--- lua/triptych/autocmds.lua
+++ lua/triptych/autocmds.lua
@@ -1,4 +1,7 @@
-local au_group = vim.api.nvim_create_augroup('TriptychAutoCmd', { clear = true })
+local au_group_internal = vim.api.nvim_create_augroup('TriptychEventsInternal', { clear = true })
+local au_group_public = vim.api.nvim_create_augroup('TriptychEvents', { clear = true })
+
+local M = {}
 
 local AutoCommands = {}
 
@@ -14,7 +17,7 @@ function AutoCommands.new(event_handlers, State, Diagnostics, Git)
 
   instance.autocmds = {
     vim.api.nvim_create_autocmd('CursorMoved', {
-      group = au_group,
+      group = au_group_internal,
       buffer = vim.api.nvim_win_get_buf(State.windows.current.win),
       callback = function()
         event_handlers.handle_cursor_moved(State)
@@ -22,14 +25,14 @@ function AutoCommands.new(event_handlers, State, Diagnostics, Git)
     }),
 
     vim.api.nvim_create_autocmd('BufLeave', {
-      group = au_group,
+      group = au_group_internal,
       buffer = vim.api.nvim_win_get_buf(State.windows.current.win),
       callback = event_handlers.handle_buf_leave,
     }),
 
     -- User autocmd for asynchronously handling the result a directory read
     vim.api.nvim_create_autocmd('User', {
-      group = au_group,
+      group = au_group_internal,
       pattern = 'TriptychPathRead',
       callback = function(data)
         event_handlers.handle_dir_read(State, data.data.path_details, data.data.win_type, Diagnostics, Git)
@@ -38,7 +41,7 @@ function AutoCommands.new(event_handlers, State, Diagnostics, Git)
 
     -- User autocmd for asynchronously handling the result a file read
     vim.api.nvim_create_autocmd('User', {
-      group = au_group,
+      group = au_group_internal,
       pattern = 'TriptychFileRead',
       callback = function(data)
         event_handlers.handle_file_read(data.data.child_win_buf, data.data.path, data.data.lines)
@@ -49,6 +52,8 @@ function AutoCommands.new(event_handlers, State, Diagnostics, Git)
   return instance
 end
 
+M.new = AutoCommands.new
+
 function AutoCommands:destroy_autocommands()
   local vim = _G.triptych_mock_vim or vim
   for _, autocmd in pairs(self.autocmds) do
@@ -56,21 +61,43 @@ function AutoCommands:destroy_autocommands()
   end
 end
 
+local function exec_autocmd(group, pattern, data)
+  vim.schedule(function()
+    vim.api.nvim_exec_autocmds('User', {
+      group = group,
+      pattern = pattern,
+      data = data,
+    })
+  end)
+end
+
+local function exec_internal_autocmd(pattern, data)
+  vim.schedule(function()
+    vim.api.nvim_exec_autocmds('User', {
+      group = au_group_internal,
+      pattern = pattern,
+      data = data,
+    })
+  end)
+end
+
+local function exec_public_autocmd(pattern, data)
+  vim.api.nvim_exec_autocmds('User', {
+    group = au_group_public,
+    pattern = pattern,
+    data = data,
+  })
+end
+
 ---Publish the results of an async directory read
 ---@param path_details PathDetails
 ---@param win_type WinType
 ---@return nil
-local function send_path_read(path_details, win_type)
-  vim.schedule(function()
-    vim.api.nvim_exec_autocmds('User', {
-      group = au_group,
-      pattern = 'TriptychPathRead',
-      data = {
-        path_details = path_details,
-        win_type = win_type,
-      },
-    })
-  end)
+function M.send_path_read(path_details, win_type)
+  exec_internal_autocmd('TriptychPathRead', {
+    path_details = path_details,
+    win_type = win_type,
+  })
 end
 
 ---Publish the results of an async file read
@@ -78,23 +105,61 @@ end
 ---@param path string
 ---@param lines string[]
 ---@return nil
-local function send_file_read(child_win_buf, path, lines)
-  vim.schedule(function()
-    vim.api.nvim_exec_autocmds('User', {
-      group = au_group,
-      pattern = 'TriptychFileRead',
-      data = {
-        child_win_buf = child_win_buf,
-        path = path,
-        lines = lines,
-      },
-    })
-  end)
+function M.send_file_read(child_win_buf, path, lines)
+  exec_internal_autocmd('TriptychFileRead', {
+    child_win_buf = child_win_buf,
+    path = path,
+    lines = lines,
+  })
 end
 
-return {
-  new = AutoCommands.new,
-  send_path_read = send_path_read,
-  send_file_read = send_file_read,
-  au_group = au_group,
-}
+-- NOTE: "publish" functions are intended as public hooks.
+-- Don't use these to add any internal logic (like with the "send" functions)
+
+---@param path string
+function M.publish_will_delete_node(path)
+  exec_public_autocmd('TriptychWillDeleteNode', {
+    path = path,
+  })
+end
+
+---@param path string
+function M.publish_did_delete_node(path)
+  exec_public_autocmd('TriptychDidDeleteNode', {
+    path = path,
+  })
+end
+
+---@param path string
+function M.publish_will_create_node(path)
+  exec_public_autocmd('TriptychWillCreateNode', {
+    path = path,
+  })
+end
+
+---@param path string
+function M.publish_did_create_node(path)
+  exec_public_autocmd('TriptychDidCreateNode', {
+    path = path,
+  })
+end
+
+---@param from_path string
+---@param to_path string
+function M.publish_will_move_node(from_path, to_path)
+  exec_public_autocmd('TriptychWillMoveNode', {
+    from_path = from_path,
+    to_path = to_path,
+  })
+end
+
+---@param from_path string
+---@param to_path string
+function M.publish_did_move_node(from_path, to_path)
+  exec_public_autocmd('TriptychDidMoveNode', {
+    from_path = from_path,
+    to_path = to_path,
+  })
+end
+
+return M
