b3b92f536359f78dbb73628167c68c2e3c79faf1
Simon McLean <simonmclean@192.168.1.116>
2024-08-31 16:00:05 +0100

    first test working

--- tests/init.lua
+++ tests/init.lua
@@ -1,57 +1,91 @@
 local u = require 'tests.utils'
 local api = vim.api
 
-local function go()
+local function open_and_get_state(callback)
   local cwd = vim.fn.getcwd()
   local opening_dir = u.join_path(cwd, 'tests/test_playground/level_1/level_2/level_3')
-  local triptych = require 'triptych'
-  triptych.setup {
-    options = {
-      file_icons = {
-        enabled = false -- Makes for easier testing
-      }
-    }
-  }
-  triptych.toggle_triptych(opening_dir)
-  local all_windows = api.nvim_list_wins()
-  local wins = {
-    child = -1,
-    primary = -1,
-    parent = -1,
-  }
-  -- TODO: Rather than waiting ticks, maybe use public events?... e.g. 'TriptychOpened'
-  u.wait_ticks(10, function()
-    for _, win in ipairs(all_windows) do
-      local has_role, role = pcall(api.nvim_win_get_var, win, 'triptych_role')
-      if has_role then
-        wins[role] = win
-      end
-    end
 
-    local bufs = {
-      child = api.nvim_win_get_buf(wins.child),
-      primary = api.nvim_win_get_buf(wins.primary),
-      parent = api.nvim_win_get_buf(wins.parent),
-    }
-
-    local lines = {
-      child = u.get_lines(bufs.child),
-      primary = u.get_lines(bufs.primary),
-      parent = u.get_lines(bufs.parent),
-    }
-
-    vim.print {
-      wins = wins,
-      bufs = bufs,
-      lines = lines
-    }
+  u.open_triptych(opening_dir)
+  u.on_all_wins_updated(function()
+    callback(u.get_state())
   end)
 end
 
-go()
+-- open_and_get_state(vim.print)
 
--- describe('UI', function()
---   it('populates windows with files and folders', function()
---     go()
---   end)
--- end)
+describe('UI', function()
+  it('when opened - populates windows with files and folders', function()
+    local expected_lines = {
+      child = { 'level_5/', 'level_4_file_1.js' },
+      primary = { 'level_4/', 'level_3_file_1.js' },
+      parent = { 'level_3/', 'level_2_file_1.js' },
+    }
+
+    local expected_winbars = {
+      child = '%#WinBar#%=%#WinBar#level_4/%=',
+      primary = '%#WinBar#%=%#WinBar#level_3%=',
+      parent = '%#WinBar#%=%#WinBar#level_2%=',
+    }
+
+    local result
+
+    u.await(function(done)
+      open_and_get_state(function(state)
+        result = state
+        done()
+      end)
+    end)()
+
+
+    assert.same(expected_lines, result.lines)
+    assert.same(expected_winbars, result.winbars)
+  end)
+
+  it('navigates up and down the filesystem', function ()
+
+  end)
+
+  it('closes on key press', function ()
+
+  end)
+
+  it('closes on command', function ()
+
+  end)
+
+  it('shows files previews', function ()
+
+  end)
+
+  it('shows a help screen', function ()
+
+  end)
+
+  it('creates files and dirs', function ()
+
+  end)
+
+  it('deletes files and dirs', function ()
+
+  end)
+
+  it('does copy-paste', function ()
+
+  end)
+
+  it('does cut-and-paste', function ()
+
+  end)
+
+  it('renames files and dirs', function ()
+
+  end)
+
+  it('jumps to cwd and back', function ()
+
+  end)
+
+  it('toggles hidden files', function ()
+
+  end)
+end)
--- tests/utils.lua
+++ tests/utils.lua
@@ -1,9 +1,11 @@
+local api = vim.api
+
 local M = {}
 
 ---@param ... string
 ---@return string
 function M.join_path(...)
-    return table.concat({ ... }, "/")
+  return table.concat({ ... }, '/')
 end
 
 ---@param n integer
@@ -22,6 +24,96 @@ end
 
 function M.get_lines(buf)
   return vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+end
+
+function M.get_winbar(win)
+  local winbar
+  api.nvim_win_call(win, function ()
+    winbar = vim.wo.winbar
+  end)
+  return winbar
+end
+
+function M.on_event(event, callback)
+  vim.api.nvim_create_autocmd('User', {
+    group = 'TriptychEvents',
+    pattern = event,
+    callback = callback,
+  })
+end
+
+function M.on_all_wins_updated(callback)
+  local wins_updated = {
+    child = false,
+    primary = false,
+    parent = false,
+  }
+  M.on_event('TriptychDidUpdateWindow', function(data)
+    wins_updated[data.data.win_type] = true
+    if wins_updated.child and wins_updated.primary and wins_updated.primary then
+      callback()
+    end
+  end)
+end
+
+function M.open_triptych(opening_dir)
+  local triptych = require 'triptych.init'
+  triptych.setup {
+    options = {
+      file_icons = {
+        enabled = false, -- Makes for easier testing
+      },
+    },
+  }
+  triptych.toggle_triptych(opening_dir)
+end
+
+function M.get_state()
+  local all_windows = api.nvim_list_wins()
+  local wins = {
+    child = -1,
+    primary = -1,
+    parent = -1,
+  }
+  for _, win in ipairs(all_windows) do
+    local has_role, role = pcall(api.nvim_win_get_var, win, 'triptych_role')
+    if has_role then
+      wins[role] = win
+    end
+  end
+
+  local bufs = {
+    child = api.nvim_win_get_buf(wins.child),
+    primary = api.nvim_win_get_buf(wins.primary),
+    parent = api.nvim_win_get_buf(wins.parent),
+  }
+
+  local lines = {
+    child = M.get_lines(bufs.child),
+    primary = M.get_lines(bufs.primary),
+    parent = M.get_lines(bufs.parent),
+  }
+
+  local winbars = {
+    child = M.get_winbar(wins.child),
+    primary = M.get_winbar(wins.primary),
+    parent = M.get_winbar(wins.parent),
+  }
+
+  return {
+    wins = wins,
+    bufs = bufs,
+    lines = lines,
+    winbars = winbars
+  }
+end
+
+function M.await(fn)
+  local co = coroutine.running()
+  fn(function()
+    coroutine.resume(co)
+  end)
+  return coroutine.yield
 end
 
 return M
