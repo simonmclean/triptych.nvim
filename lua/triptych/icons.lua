513fc9f26d9f0b294f2afc1e2f4573e82ff072f2
dladuke <dladukedev@gmail.com>
2025-05-16 23:21:31 -0400

    update logic to load icon from name and extension

--- lua/triptych/icons.lua
+++ lua/triptych/icons.lua
@@ -4,12 +4,15 @@
 local devicons_installed, devicons = pcall(require, 'nvim-web-devicons')
 
 return {
-  ---@param filetype string?
+  ---@param filename string?
   ---@return string? icon
   ---@return string? highlight
-  get_icon_by_filetype = function(filetype)
-    if devicons_installed and filetype then
-      local icon, highlight = devicons.get_icon_by_filetype(filetype)
+  get_icon_by_filename = function(filename)
+    if devicons_installed and filename then
+      -- Capture all characters after the last dot
+      local file_ext = filename:match('^.*%.(.*)$')
+
+      local icon, highlight = devicons.get_icon(filename, file_ext)
       return icon, highlight
     end
     return nil, nil
